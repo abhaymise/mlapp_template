94a3368b3e0097397836fd1d4d47a7b94321263b
abhaymise <abhay8280@gmail.com>
2023-09-21 11:30:39 +0530

    first commit with all structure

---
+++ app/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Module desciption: 
+Description of what __init__.py does.
+"""
+# __init__.py created at 21-09-2023
+
+
+if __name__ == '__main__':
+    pass
---
+++ mlapp/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Module desciption: 
+Description of what __init__.py does.
+"""
+# __init__.py created at 21-09-2023
+__author__ = "Abhay Kumar"
+__date__ = "21-09-2023"
+__copyright__ = "Copyright 2022"
+__credits__ = []
+__license__ = ""
+__version__ = "0.1"
+__maintainer__ = ""
+__email__ = ""
+__status__ = "POC"
+
+if __name__ == '__main__':
+    pass
